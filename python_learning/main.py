e23584c4db4fa7f0dcccb82f6ef5e26a0f632c46
tejas <pangaonkar.svc424@gmail.com>
2025-09-23 18:28:51 +0530

    init

---
+++ python_learning/main.py
@@ -0,0 +1,57 @@
+# print("Hello, world")
+
+# print(type("We"))
+
+# age = 20
+
+# if age < 20:
+#     print("less than 20")
+# elif age >= 20 and age < 21:
+#     print("in between 20 and 21")
+# else : 
+#     print("more than 20")
+
+# count = 0
+# while count < 5:
+#     print(count)
+#     count+=1
+
+# for i in range(5):
+#     print(i)
+
+# for ch in "Python":
+#     print(ch)
+
+# for num in range(10):
+#     if num % 2 == 0:
+#         continue
+#     if num == 9:
+#         break
+#     print(num)
+
+# num = int(input("Enter a number"))
+
+# if num % 2 == 0:
+#     print("even")
+# else :
+#     print("odd")
+
+import random
+secret = random.randint(1,100)
+# secret = random.seed(42)
+
+print(secret)
+guess = None
+
+while secret != guess :
+    guess = int(input("Enter a number"))
+
+if secret == guess:
+    print("correct guess")
+elif guess > secret:
+    print("too high")
+elif guess < secret:
+    print("too low")
+else:
+    print("wrong input")
+
